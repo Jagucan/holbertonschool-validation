2bb18807f1e5b94989dff1767eef11fabd9b1070
Jagucan <jjgc.2499@gmail.com>
2023-05-14 11:04:58 -0600

    task 1-2

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -7,4 +7,4 @@ rm hugo_binary.deb
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
 npm install -g markdown-link-check
 npm install -g markdownlint-cli
-
+make build
