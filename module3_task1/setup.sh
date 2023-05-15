7e901f6597d371426335ae97fd064c00a88f7e95
Jagucan <jjgc.2499@gmail.com>
2023-05-15 17:47:48 -0600

    commit 2

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt update && apt upgrade -y
 apt install -y wget make dpkg
-wget -O hugo_binary.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
-dpkg -i hugo_binary.deb
-rm hugo_binary.deb
+wget -O hugo_extended_0.111.3.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
+dpkg -i hugo_extended_0.111.3.deb
+rm hugo_extended_0.111.3.deb
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
 export PATH=$PATH:/usr/local/go/bin
-npm install -g markdown-link-check
 npm install -g markdownlint-cli
+npm install -g markdown-link-check
