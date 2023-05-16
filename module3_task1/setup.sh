eebb8cdda8e293a77b47368bdd2925793c4c6826
Jagucan <jjgc.2499@gmail.com>
2023-05-15 18:48:41 -0600

    commit

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -2,7 +2,7 @@
 apt update && apt upgrade -y
 apt install -y wget make dpkg
 wget -O hugo_extended_0.111.3.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
-dpkg -i hugo_extended_0.111.3.deb
+sudo dpkg -i hugo_extended_0.111.3.deb
 rm hugo_extended_0.111.3.deb
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
 export PATH=$PATH:/usr/local/go/bin
