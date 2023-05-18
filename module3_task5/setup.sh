4d281b454b39b66088f3d274e35a7e793636c0e7
Jagucan <jjgc.2499@gmail.com>
2023-05-18 12:23:55 -0500

    task 5

--- module3_task5/setup.sh
+++ module3_task5/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update && apt upgrade -y
-apt install -y wget make dpkg
+apt install -y wget make dpkg curl
 wget -O hugo_extended_0.111.3.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
 sudo dpkg -i hugo_extended_0.111.3.deb
 rm hugo_extended_0.111.3.deb
