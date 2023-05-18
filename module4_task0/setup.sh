e773e94a3a8ff72eb32e6fa417fb29a6d0c87781
Jagucan <jjgc.2499@gmail.com>
2023-05-18 16:03:32 -0500

    commit

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -4,5 +4,5 @@ apt install -y wget make dpkg
 wget -O hugo_extended_0.111.3.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
 sudo dpkg -i hugo_extended_0.111.3.deb
 rm hugo_extended_0.111.3.deb
-npm install -g markdownlint-cli
-npm install -g markdown-link-check
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
