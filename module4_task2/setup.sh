a1ba5139ef36716da0c61d02a205dbbf3fe32576
Jagucan <jjgc.2499@gmail.com>
2023-05-22 12:51:10 -0500

    task 4-2 1

--- module4_task2/setup.sh
+++ module4_task2/setup.sh
@@ -4,6 +4,7 @@ sudo apt install -y wget make dpkg curl
 wget -O hugo_extended_0.111.3.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
 sudo dpkg -i hugo_extended_0.111.3.deb
 rm hugo_extended_0.111.3.deb
+curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 && chmod +x container-structure-test-linux-amd64 && sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
 export PATH=$PATH:/usr/local/go/bin
 sudo npm install -g npm@9.6.7
