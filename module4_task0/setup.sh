180f57e7cf016e187c5e3a53ad6ef5f3ef3d35e1
Jagucan <jjgc.2499@gmail.com>
2023-05-24 12:17:32 -0500

    task 4-0 2

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -4,7 +4,8 @@ sudo apt install -y wget make dpkg curl
 wget -O hugo_extended_0.111.3.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
 sudo dpkg -i hugo_extended_0.111.3.deb
 rm hugo_extended_0.111.3.deb
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
+golangci-lint --version
 export PATH=$PATH:/usr/local/go/bin
 sudo npm install -g npm@9.6.7 && npm install -g markdownlint-cli && sudo npm install -g markdown-link-check
 sudo apt-get install zip -y
